025eab6358272c07a68f0c2c41ea18c573a2351e
Om <omhankare001@gmail.com>
2025-06-28 16:30:30 +0530

    ui change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/exxam.io-UI/",
+  base: "/exxam.io/",
   plugins: [react(), tailwindcss()],
 });
